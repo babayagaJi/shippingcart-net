eaf1892d3daf6a676b07b40defea34ebb3ccf822
Shubham <shubhamgupta2510188@gmail.com>
2023-06-16 22:49:25 +0530

    first commit

---
+++ ShoppingCart/Models/DB_Entities.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ShoppingCart.Models
+{
+    public class DB_Entities : DbContext
+    {
+        public DB_Entities() : base("DatabaseMVC5") { }
+        public DbSet<User> Users { get; set; }
+
+        /*
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+        */
+    }
+}
